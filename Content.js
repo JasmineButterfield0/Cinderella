cc9b7da9d0f53d51293c88aa0471d2b7c9239ab9
JasmineButterfield0 <ja895969@ucf.edu>
2025-02-04 22:23:27 -0500

    src

---
+++ Content.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Content = () => {
+  return (
+    <main>
+      <p>
+        Once upon a time there was a girl named Cinderella who had an abusive stepmother and two stepdaughters that love to bully Cinderella. One day, Cinderella stepmother and her step sisters are invited a royal ball but Cinderella can't go to this ball becuase she has to complete this long list her stepmother left for her while they're out at the ball. Cinderella wishes she can go to ball thats when Cinderellas fairy godmother shows up ans grants Cinderellas wish to go to the ball.Cinderellas fairy godmother used her magic to provide Cinderella with a dress, servants to help her, and a glass slipper. The first night of the ball, Cinderella makes back home in time before midnight when the spell is breaks.
+        </p>
+        <p>
+         The second night of the ball, she leaves in a hurry when it's midnight. While she was hurrying back home she drops one of her glass slippers. The prince finds the shoe and searches to find the mystery women who lost their glass slipper. After Cinderella is discovered by the prince. They get married and live happily ever after
+    </p>
+    </main>
+  );
+};
+
+export default Content;
---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>The Little Glass Slipper</p>
+    </footer>
+  );
+};
+
+export default Footer;
